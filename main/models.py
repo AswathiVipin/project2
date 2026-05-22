72a0bcdfb9b9c39cd4d6d56b5a9b3a2b832ff2b7
AswathiVipin <aswathivipi2016@gmail.com>
2026-05-22 12:13:14 +0530

    test file add, settings updtaed

--- main/models.py
+++ main/models.py
@@ -11,3 +11,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
